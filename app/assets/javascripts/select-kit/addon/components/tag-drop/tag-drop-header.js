d178c726956444a2ecf508fe777e82841837e1a9
Penar Musaraj <pmusaraj@gmail.com>
2022-06-22 21:50:00 -0400

    Add clearable button to tags too

--- app/assets/javascripts/select-kit/addon/components/tag-drop/tag-drop-header.js
+++ app/assets/javascripts/select-kit/addon/components/tag-drop/tag-drop-header.js
@@ -1,7 +1,9 @@
 import ComboBoxSelectBoxHeaderComponent from "select-kit/components/combo-box/combo-box-header";
 import layout from "select-kit/templates/components/tag-drop/tag-drop-header";
+import { bool } from "@ember/object/computed";
 
 export default ComboBoxSelectBoxHeaderComponent.extend({
   layout,
   classNames: "tag-drop-header",
+  shouldDisplayClearableButton: bool("value"),
 });
