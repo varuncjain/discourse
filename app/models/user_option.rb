208cff5650a87857c5f492bb687cf331d14741f1
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-09-28 00:27:02 +0300

    Apply code suggestions

--- app/models/user_option.rb
+++ app/models/user_option.rb
@@ -268,6 +268,10 @@ end
 #  oldest_search_log_date           :datetime
 #  bookmark_auto_delete_preference  :integer          default(3), not null
 #  enable_experimental_sidebar      :boolean          default(FALSE)
+#  skip_first_notification          :boolean          default(FALSE)
+#  skip_post_menu                   :boolean          default(FALSE)
+#  skip_topic_timeline              :boolean          default(FALSE)
+#  skip_user_card                   :boolean          default(FALSE)
 #
 # Indexes
 #
